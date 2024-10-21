d58972488e9ba02cb3a855f3c07ed9e7563e23a4
Rameez Ali <83869493+ramizali4@users.noreply.github.com>
2024-10-21 13:57:11 +0500

    second commit

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+string consumeNumber()
+    // {
+    //     size_t start = pos;
+    //     while (pos < src.size() && isdigit(src[pos]))
+    //     {
+    //         pos++;
+    //     }
+    //     return src.substr(start, pos - start);
+    // }
